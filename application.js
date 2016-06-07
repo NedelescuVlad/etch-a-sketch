7406af2800ebb5874ad2b820885aae0d99179c6d
Vlad C. Nedelescu <nedelescu.vlad@gmail.com>
2016-06-07 23:32:13 +0300

    Squares are randomly colored upon hover

--- application.js
+++ application.js
@@ -2,23 +2,64 @@ $(document).ready(function() {
 
 	drawGrid(16);
 
+	$("button").click(function() {
+
+		var response = prompt("What size should the grid be?");
+		// substring the response in case the user typed e.g. 12x12 instead of 12
+		drawGrid(response.substring(0, 3));
+
+	});
+
 });
 
 function drawGrid(n) {
 
+	$("body").children("div").remove();
+
 	for (i = 0; i < n * n; i++) {
-		$("body").prepend($("<div class='square'></div>"));
+		var square = new Object();
+		square.div = $("<div class='square'></div>");
+		square.color = "rgb(255, 255, 255)";
+		square.wasHovered = false;
+		square.div.insertAfter($("button"));
 	}
 
 	var squares = $("body").children("div");
-	var square_width =  100/n + "%";
-	var square_height = $(window).height() / n;
 
+	// Handle the mouseenter event
+	$(squares).on({
+    mouseenter: function() {
+
+    	if(!$(this).hasClass('transition'))
+     		$(this).addClass('transition');
+
+      $(this).css("background-color", getRandomRGB);
+    }
+   });
+
+	// Calculate the width and height for a square
+	var square_width =  100 / n + "%";
+	var square_height = (+$(window).height() - (+$("button").height())) / n;
+
+	// Set that width and height
 	squares.css({"width":square_width, "height":square_height});
 
-	squares.on('mouseenter', function() {
+}
 
-		$(this).addClass('highlight');
+// Returns a random rgb color
+function getRandomRGB() {
 
-	});
+    var red = getRandomColor() + ", ";
+    var green = getRandomColor() + ", ";
+    var blue = getRandomColor() + ")";
+
+    var color = 'rgb(' + red + green + blue;
+    
+    return color;
+
+}
+
+// Returns a random value between 0 - 255 representing a red / green / blue value for rgb
+function getRandomColor() {
+	return Math.floor(Math.random() * 256);
 }
